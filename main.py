10ac19166f584c759191b7a1397ae25acde10a87
jackinab0x <31683388+jackinab0x@users.noreply.github.com>
2018-01-30 18:58:38 +0530

    Added report and first code

---
+++ main.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 30 16:01:01 2018
+
+@author: jackinab0x
+"""
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+import numpy as np
+
